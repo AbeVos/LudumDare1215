9e22402ecb699e36bdf998a19395706f78632483
Mathijs Lemmers <mathijs750@gmail.com>
2015-12-13 00:23:33 +0100

    slaap lekker

--- Assets/Scripts/Gameplay/Dragon.cs
+++ Assets/Scripts/Gameplay/Dragon.cs
@@ -3,14 +3,42 @@ using System.Collections;
 
 public class Dragon : MonoBehaviour, GameActor
 {
+    private enum WeaponState
+    {
+        /// <summary>Idle state, Initial state.</summary>
+        Idle,
+        /// <summary>Attack active, heat rising</summary>
+        PrimaryActive,
+        /// <summary>Charge is building. Weapon is off</summary>
+        SecondaryActive,
+        /// <summary>Charge is empty, attack is active </summary>
+        SecondaryFired
+    }
+    #region Variables
     [SerializeField, Range(4, 10)]
-    private float responseSpeed = 5f;
+    private float responseSpeed = 7.5f;
 
+    [Header("Weapon Speeds")]
+    [SerializeField, Range(10, 30)]
+    private float chargeSpeed = 10;
+    [SerializeField, Range(1, 30)]
+    private float heatUpSpeed = 10;
+    [SerializeField, Range(1, 30)]
+    private float coolDownSpeed = 10;
+
+
+    /// <summary>Heat is at 100%, primary can only start again at 0% heat</summary>
+    private bool primaryOverheat;
+    private WeaponState currentState;
     private Vector3 lastWorldPosition;
+    #endregion
 
+    #region statics
     /// <summary>Static reference to self.</summary>
     private static Dragon self;
-    private static int _health, _charge, _heat, _exp;
+    private static int _health, _exp = 0;
+    private static float _charge, _heat = 0f;
+    #endregion
 
     #region Properties
     public static Dragon dragon
@@ -22,28 +50,26 @@ public class Dragon : MonoBehaviour, GameActor
         get { return _health; }
         set { _health = value; }
     }
-    public static int Epx
+    public static int Exp
     {
         get { return _exp; }
         set { _exp = value; }
     }
-    public static int Charge
+    public static float Charge
     {
         get { return _charge; }
         private set
         {
-            if (value <= 100 && value <= 0) { _charge = value; }
+            if (value <= 100 && value >= 0) { _charge = value; }
         }
-
     }
-    public static int Heat
+    public static float Heat
     {
-        get { return _health; }
+        get { return _heat; }
         private set
         {
-            if (value <= 100 && value <= 0) { _health = value; }
+            if (value <= 100 && value >= 0) { _heat = value; }
         }
-
     }
     #endregion
 
@@ -72,6 +98,30 @@ public class Dragon : MonoBehaviour, GameActor
             transform.position = Vector3.Lerp(lastWorldPosition, Camera.main.ScreenToWorldPoint(InputManager.MousePosition), Time.deltaTime * responseSpeed);
             lastWorldPosition = transform.position;
         }
+
+        
+
+        switch (currentState)
+        {
+            case WeaponState.Idle:
+                Heat -= Time.deltaTime * coolDownSpeed;
+                Charge = 0;
+                break;
+            case WeaponState.PrimaryActive:
+                FirePrimary();
+                Heat += Time.deltaTime * heatUpSpeed;
+                Charge = 0;
+                break;
+            case WeaponState.SecondaryFired:
+                Charge = 0;
+                SetState( WeaponState.Idle);
+                break;
+            case WeaponState.SecondaryActive:
+                Heat -= Time.deltaTime * coolDownSpeed;
+                Charge += Time.deltaTime * chargeSpeed;
+                break;
+        }
+
     }
 
     void OnDisable()
@@ -83,27 +133,60 @@ public class Dragon : MonoBehaviour, GameActor
     {
     }
 
+    private void SetState(WeaponState newState)
+    {
+        WeaponState prevState = currentState;
+        currentState = newState;
+        Debug.Log(newState);
+    }
+
+
+    public void Hit(int damage)
+    {
+        Health -= damage;
+    }
+
+    private void FirePrimary()
+    {
+        Debug.Log("Primary");
+    }
+
+    private void FireSecondary()
+    {
+        Debug.Log("Secondary");
+    }
+
+    #region Buttons
     private void InputManager_OnPrimaryButtonDown()
     {
-        Debug.Log("Prime Down");
+        if (!primaryOverheat && currentState == WeaponState.Idle)
+        {
+            SetState(WeaponState.PrimaryActive);
+        }
     }
 
     private void InputManager_OnPrimaryButtonUp()
     {
-        Debug.Log("Prime UP");
+        SetState(WeaponState.Idle);
     }
 
     private void InputManager_OnSecondaryButtonDown()
     {
-        Debug.Log("Second DOWN");
+        if (Charge <= 0)
+        {
+            SetState(WeaponState.SecondaryActive); 
+        }
     }
 
     private void InputManager_OnSecondaryButtonUp()
     {
-        Debug.Log("Second UP");
-    }
+        SetState(WeaponState.Idle);
 
-    public void Hit(int damage)
-    {
+        if ( Charge > 95f)
+        {
+            FireSecondary();
+            SetState(WeaponState.SecondaryFired);
+        }
     }
+    #endregion
 }
---
+++ Assets/Scripts/Technical/UIController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIController : MonoBehaviour
+{
+    private Slider Epx, Heat, Charge;
+
+    void Start ()
+    {
+        Epx = transform.GetChild(1).GetComponent<Slider>();
+        Heat = transform.GetChild(2).GetComponent<Slider>();
+        Charge = transform.GetChild(3).GetComponent<Slider>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Epx.value = Dragon.Exp;
+        Heat.value = Dragon.Heat;
+        Charge.value = Dragon.Charge;
+	}
+}
