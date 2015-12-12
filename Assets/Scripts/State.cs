870b96015ce1eaf1e1fc02fd7b7b3d0e57b98b41
Abe Vos <abe_vos@msn.com>
2015-12-12 11:04:20 +0100

    Lege objecten omdat unity moeilijk doet

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Global Game Manager
+/// </summary>
+public class State : MonoBehaviour
+{
+    public enum GlobalState
+    {
+        /// <summary>Game Introduction.</summary>
+        Start,
+        /// <summary>Setting up stage (spawning enemies etc.).</summary>
+        Initialize,
+        /// <summary>Gameplay</summary>
+        Game,
+        /// <summary>(Optional) Game won.</summary>
+        Win,
+        /// <summary>Game lost.</summary>
+        Lose,
+        /// <summary>Game Over, move on to new scene.</summary>
+        End
+    }
+
+    public delegate void OnGameStateChangedHandle(GlobalState prevGlobalState, GlobalState newGlobalState);
+    public static event OnGameStateChangedHandle OnGameStateChanged;
+
+    private static GlobalState globalState;
+
+    public static GlobalState Current
+    {
+        get { return globalState; }
+    }
+
+    public static void ChangeState (GlobalState newState)
+    {
+        GlobalState prevState = globalState;
+        globalState = newState;
+
+        Debug.Log("Changed Global State from " + prevState.ToString() + " to " + newState.ToString());
+
+        if (OnGameStateChanged != null) OnGameStateChanged(prevState, newState);
+    }
+}
