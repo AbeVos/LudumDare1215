1ac04798930e7fac64dddc6bd87b46c250320149
Abe Vos <abe_vos@msn.com>
2015-12-13 09:40:03 +0100

    Started on ObjectPool

---
+++ Assets/Scripts/Technical/ObjectPool.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectPool : MonoBehaviour
+{
+    struct Bullet
+    {
+        GameObject bullet;
+        Vector3 velocity;
+    }
+}
