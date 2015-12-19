dfeb4cb611eb9a29e451eb925017f06a47fd35b4
Mathijs Lemmers <mathijs750@gmail.com>
2015-12-19 01:48:05 +0100

    Remove unused debug log

--- Assets/Scripts/Gameplay/Dragon.cs
+++ Assets/Scripts/Gameplay/Dragon.cs
@@ -340,7 +340,7 @@ public class Dragon : MonoBehaviour, GameActor
             coolDownSpeed = upgrade.PrimaryCooldown;
             FireRatePrimary = upgrade.PrimaryFireRate;
         }
-        Debug.Log("> Did upgrade");
+ //       Debug.Log("> Did upgrade");
     }
     #endregion
 
